1be07907d3c97144a1ee4786a614cb1b2cabaec0
navewindre <boneyaard@gmail.com>
2017-10-17 01:40:34 +0200

    Update Source.cpp

--- interface_factory/Project8/Source.cpp
+++ interface_factory/Project8/Source.cpp
@@ -2,6 +2,7 @@
 
 bool __stdcall DllMain( HINSTANCE inst, ulong_t reason, void* reserved ) {
 	if ( reason == DLL_PROCESS_ATTACH ) {
+		DisableThreadLibraryCalls( inst );
 		g_factory.dump_interface_list( );
 	}
 
