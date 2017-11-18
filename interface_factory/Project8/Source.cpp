155e129d84122d267cd141802cf6c3072e4f68a4
terryadavis <33110055+terryadavis@users.noreply.github.com>
2017-11-18 08:02:55 -0800

    Update Source.cpp
    fix for "special" nave coding style

--- interface_factory/Project8/Source.cpp
+++ interface_factory/Project8/Source.cpp
@@ -4,7 +4,7 @@ bool __stdcall DllMain( HINSTANCE instance, ulong_t reason, void* reserved ) {
     switch ( reason ) {
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls( instance );
-        g_factory.dump_interface_list();
+        g_factory.dump_interface_list( );
 
         return true;
         break;
